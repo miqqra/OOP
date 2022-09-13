95b97dfdac42d579557f997aa72ceaf08350a0d5
miqqra <mikhail.krasnikov.04@gmail.com>
2022-09-13 21:25:14 +0700

    corrected codestyle

--- Task_1_1_1/src/main/java/ru/nsu/krasnikov/Heap.java
+++ Task_1_1_1/src/main/java/ru/nsu/krasnikov/Heap.java
@@ -20,8 +20,9 @@ public class Heap {
     }
 
     private void siftUp(int[] array, int idx) {
-        if (idx == 0 || (array[idx] >= array[(idx - 1) / 2]))
+        if (idx == 0 || (array[idx] >= array[(idx - 1) / 2])) {
             return;
+        }
         else {
             swap(array, idx, (idx - 1) / 2);
             siftUp(array, (idx - 1) / 2);
@@ -36,17 +37,22 @@ public class Heap {
 
     private void siftDown(int[] array, int elemIndex, int currentLength) {
         int swapIndex;
-        if (2 * elemIndex + 1 > currentLength)
+        if (2 * elemIndex + 1 > currentLength) {
             return;
-        else if (2 * elemIndex + 1 == currentLength)
+        }
+        else if (2 * elemIndex + 1 == currentLength) {
             swapIndex = currentLength;
-        else if (array[2 * elemIndex + 1] <= array[2 * elemIndex + 2])
+        }
+        else if (array[2 * elemIndex + 1] <= array[2 * elemIndex + 2]) {
             swapIndex = 2 * elemIndex + 1;
-        else
+        }
+        else {
             swapIndex = 2 * elemIndex + 2;
+        }
 
-        if (array[elemIndex] <= array[swapIndex])
+        if (array[elemIndex] <= array[swapIndex]) {
             return;
+        }
         else {
             swap(array, elemIndex, swapIndex);
             siftDown(array, swapIndex, currentLength - 1);
