49951b96dc8aa6de7d06011cc56e3c8ad7c464f1
miqqra <mikhail.krasnikov.04@gmail.com>
2022-09-13 20:14:16 +0700

    added tests

--- Task_1_1_1/src/test/java/ru/nsu/krasnikov/HeapTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/krasnikov/HeapTest.java
@@ -1,13 +1,62 @@
 package ru.nsu.krasnikov;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class HeapTest {
-    public static void heapTest(String[] args) {
-        int[] array = {1, 1, 1, 1, 2, 3, 3, 3, 3, 1};
+
+    @Test
+    public void heapTestEmptyArray() {
+        int[] array = {};
 
         Heap heapArray = new Heap(array);
         int[] sortedArray = heapArray.heapSort();
+        int[] correct_array = {};
 
-        for (int i = 0; i < sortedArray.length; i++)
-            System.out.println(sortedArray[i] + " ");
+        Assertions.assertArrayEquals(correct_array, sortedArray);
+    }
+
+    @Test
+    public void heapTestOneElemArray() {
+        int[] array = {1};
+
+        Heap heapArray = new Heap(array);
+        int[] sortedArray = heapArray.heapSort();
+        int[] correct_array = {1};
+
+        Assertions.assertArrayEquals(correct_array, sortedArray);
+    }
+
+    @Test
+    public void heapTestTwoElemsArray() {
+        int[] array = {2,1};
+
+        Heap heapArray = new Heap(array);
+        int[] sortedArray = heapArray.heapSort();
+        int[] correct_array = {1,2};
+
+        Assertions.assertArrayEquals(correct_array, sortedArray);
+    }
+
+    @Test
+    public void heapTestFourElemsArray() {
+        int[] array = {2,4,3,1};
+
+        Heap heapArray = new Heap(array);
+        int[] sortedArray = heapArray.heapSort();
+        int[] correct_array = {1,2,3,4};
+
+        Assertions.assertArrayEquals(correct_array, sortedArray);
+    }
+
+    @Test
+    public void heapTestMoreElemsArray() {
+        int[] array = {2,4,3,1,6,6,8,9,0,-10};
+
+        Heap heapArray = new Heap(array);
+        int[] sortedArray = heapArray.heapSort();
+        int[] correct_array = {-10,0,1,2,3,4,6,6,8,9};
+
+        Assertions.assertArrayEquals(correct_array, sortedArray);
     }
 }
