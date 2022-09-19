28701cf2df3ebfaf6a95ac2c95dae222b6c3efbd
miqqra <75527785+miqqra@users.noreply.github.com>
2022-09-19 15:59:36 +0700

    Delete Task_1_1_1 directory

--- Task_1_1_1/build.bat
+++
@@ -1,8 +0,0 @@
-mkdir .\build_project\doc\
-javadoc -d .\build_project\doc\ -charset utf-8 -sourcepath .\src\main\java\ -subpackages ru.nsu.krasnikov
-
-mkdir .\build_project\bin\
-javac -d .\build_project\bin\ .\src\main\java\ru\nsu\krasnikov\Heap.java
-
-mkdir .\build_project\jar\
-jar cf .\build_project\jar\HeapSort.jar .\src\main\java\ru\nsu\krasnikov\Heap.java
