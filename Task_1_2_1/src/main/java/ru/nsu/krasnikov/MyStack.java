605b027169d77f5682efaf9664ae1ec630b2e99f
miqqra <mikhail.krasnikov.04@gmail.com>
2022-09-19 11:36:56 +0700

    added StackInterface, changed MyStack to subclass, added Tests

--- Task_1_2_1/src/main/java/ru/nsu/krasnikov/MyStack.java
+++ Task_1_2_1/src/main/java/ru/nsu/krasnikov/MyStack.java
@@ -1,51 +1,58 @@
 package ru.nsu.krasnikov;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
-public class MyStack<E> {
+public class MyStack<E> implements StackInterface<E>{
     private int arrayLength;
     private int capacity;
-    private Object[] stack;
+    private E[] stack;
 
+    @SuppressWarnings("unchecked")
     public MyStack() {
         this.capacity = 10;
         this.arrayLength = 0;
-        this.stack = new Object[10];
+        this.stack = (E[])new Object[10];
     }
 
+    @SuppressWarnings("unchecked")
     private void resizeStack()
     {
-        Object[] newStack = new Object[capacity*2];
+        E[] newStack = (E[])new Object[capacity*2];
         System.arraycopy(stack, 0, newStack, 0, stack.length);
         stack = newStack;
         capacity*=2;
     }
 
+    @Override
     public void push(E element) {
         if (arrayLength == capacity)
         {
-            resizeStack();
+            this.resizeStack();
         }
         stack[arrayLength++] = element;
     }
 
+    @Override
     public void pushStack(E[] stack) {
         for (E element : stack) {
             this.push(element);
         }
     }
 
-    public void pop() {
-        arrayLength--;
+    @Override
+    public E pop() {
+        return stack[--arrayLength];
     }
 
-    public void popStack(int amountOfElements) {
+    @SuppressWarnings("unchecked")
+    @Override
+    public E[] popStack(int amountOfElements) {
+        E[] poppedElements = (E[])(new Object[amountOfElements]);
         for (int i = 0; i < amountOfElements; i++) {
-            this.pop();
+            poppedElements[amountOfElements-i-1] = this.pop();
         }
+        return poppedElements;
     }
 
+    @Override
     public int count() {
         return arrayLength;
     }
