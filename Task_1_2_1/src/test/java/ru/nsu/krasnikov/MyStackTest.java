cf214a2f63295f397479cd3d239b16135adcc4c9
miqqra <mikhail.krasnikov.04@gmail.com>
2022-09-24 18:19:59 +0700

    commit for reviewdog

--- Task_1_2_1/src/test/java/ru/nsu/krasnikov/MyStackTest.java
+++ Task_1_2_1/src/test/java/ru/nsu/krasnikov/MyStackTest.java
@@ -16,14 +16,14 @@ public class MyStackTest {
         f.push(2);
         Assertions.assertEquals(f.count(), 2);
         f.pushStack(new MyStack<>(new Integer[]{1, 2, 3, 4, 5, 6,
-                7, 8, 9, 10}));
+            7, 8, 9, 10}));
         Assertions.assertEquals(f.count(), 12);
         Assertions.assertEquals(f.pop(), 10);
         Assertions.assertEquals(f.count(), 11);
         Assertions.assertEquals(f.pop(), 9);
         Assertions.assertEquals(f.count(), 10);
         Assertions.assertArrayEquals(f.popStack(4),
-                new Integer[]{5, 6, 7, 8});
+            new Integer[]{5, 6, 7, 8});
         Assertions.assertEquals(f.count(), 6);
         f.pushStack(new MyStack<>(new Integer[]{}));
         Assertions.assertEquals(f.count(), 6);
@@ -39,14 +39,14 @@ public class MyStackTest {
         f.push("world");
         Assertions.assertEquals(f.count(), 2);
         f.pushStack(new MyStack<>(new String[]{"he", "ll", "o ", "wo",
-                "rl", "d ", " d", "lr", "ow", " o"}));
+            "rl", "d ", " d", "lr", "ow", " o"}));
         Assertions.assertEquals(f.count(), 12);
         Assertions.assertSame(f.pop(), " o");
         Assertions.assertEquals(f.count(), 11);
         Assertions.assertSame(f.pop(), "ow");
         Assertions.assertEquals(f.count(), 10);
         Assertions.assertArrayEquals(f.popStack(4),
-                new String[]{"rl", "d ", " d", "lr"});
+            new String[]{"rl", "d ", " d", "lr"});
         Assertions.assertEquals(f.count(), 6);
         f.pushStack(new MyStack<>(new String[]{}));
         Assertions.assertEquals(f.count(), 6);
@@ -62,14 +62,14 @@ public class MyStackTest {
         f.push(2L);
         Assertions.assertEquals(f.count(), 2);
         f.pushStack(new MyStack<>(new Long[]{123232L, 2L, 3L, 4L, 5L,
-                6L, 7L, 8L, 9L, 10L}));
+            6L, 7L, 8L, 9L, 10L}));
         Assertions.assertEquals(f.count(), 12);
         Assertions.assertEquals(f.pop(), 10L);
         Assertions.assertEquals(f.count(), 11);
         Assertions.assertEquals(f.pop(), 9L);
         Assertions.assertEquals(f.count(), 10);
         Assertions.assertArrayEquals(f.popStack(4),
-                new Long[]{5L, 6L, 7L, 8L});
+            new Long[]{5L, 6L, 7L, 8L});
         Assertions.assertEquals(f.count(), 6);
         f.pushStack(new MyStack<>(new Long[]{}));
         Assertions.assertEquals(f.count(), 6);
@@ -85,14 +85,14 @@ public class MyStackTest {
         f.push('e');
         Assertions.assertEquals(f.count(), 2);
         f.pushStack(new MyStack<>(new Character[]{'h', 'e', 'l', 'l', 'o',
-                ' ', 'w', 'o', 'r', 'l', 'd'}));
+            ' ', 'w', 'o', 'r', 'l', 'd'}));
         Assertions.assertEquals(f.count(), 13);
         Assertions.assertEquals(f.pop(), 'd');
         Assertions.assertEquals(f.count(), 12);
         Assertions.assertEquals(f.pop(), 'l');
         Assertions.assertEquals(f.count(), 11);
         Assertions.assertArrayEquals(f.popStack(4),
-                new Character[]{' ', 'w', 'o', 'r'});
+            new Character[]{' ', 'w', 'o', 'r'});
         Assertions.assertEquals(f.count(), 7);
         f.pushStack(new MyStack<>(new Character[]{}));
         Assertions.assertEquals(f.count(), 7);
@@ -108,14 +108,14 @@ public class MyStackTest {
         f.push(2.53233F);
         Assertions.assertEquals(f.count(), 2);
         f.pushStack(new MyStack<>(new Float[]{1.3F, 2.2F, 3.2F, 4.9F, 5.8F,
-                6.7F, 7.6F, 8.5F, 9.4F, 10.1F}));
+            6.7F, 7.6F, 8.5F, 9.4F, 10.1F}));
         Assertions.assertEquals(f.count(), 12);
         Assertions.assertEquals(f.pop(), 10.1F);
         Assertions.assertEquals(f.count(), 11);
         Assertions.assertEquals(f.pop(), 9.4F);
         Assertions.assertEquals(f.count(), 10);
         Assertions.assertArrayEquals(f.popStack(4),
-                new Float[]{5.8F, 6.7F, 7.6F, 8.5F});
+            new Float[]{5.8F, 6.7F, 7.6F, 8.5F});
         Assertions.assertEquals(f.count(), 6);
         f.pushStack(new MyStack<>(new Float[]{}));
         Assertions.assertEquals(f.count(), 6);
@@ -131,14 +131,14 @@ public class MyStackTest {
         f.push(false);
         Assertions.assertEquals(f.count(), 2);
         f.pushStack(new MyStack<>(new Boolean[]{true, false, true, false,
-                true, false, true, false, true, false}));
+            true, false, true, false, true, false}));
         Assertions.assertEquals(f.count(), 12);
         Assertions.assertFalse(f.pop());
         Assertions.assertEquals(f.count(), 11);
         Assertions.assertTrue(f.pop());
         Assertions.assertEquals(f.count(), 10);
         Assertions.assertArrayEquals(f.popStack(4),
-                new Boolean[]{true, false, true, false});
+            new Boolean[]{true, false, true, false});
         Assertions.assertEquals(f.count(), 6);
         f.pushStack(new MyStack<>(new Boolean[]{}));
         Assertions.assertEquals(f.count(), 6);
