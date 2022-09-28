350a93e2018dd2e6950d4b40bbb4609279a4a883
miqqra <mikhail.krasnikov.04@gmail.com>
2022-09-28 18:57:09 +0700

    3rd lab (without remove, bfs, dfs and tests)

---
+++ Task_1_2_2/src/test/java/ru/nsu/krasnikov/TreeTest.java
@@ -0,0 +1,12 @@
+package ru.nsu.krasnikov;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TreeTest {
+
+    @Test
+    public void test1(){
+
+    }
+}
