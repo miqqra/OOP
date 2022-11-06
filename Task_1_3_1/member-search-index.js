dcc52ae7a325187c444512cd879d4e6cd65d9c26
miqqra <miqqra@users.noreply.github.com>
2022-11-06 19:12:13 +0000

    Deploying to gh-pages from @ miqqra/OOP@917f727c76eb32c33450450cbeba63ee7ccb1513 🚀

---
+++ Task_1_3_1/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"ru.nsu.krasnikov","c":"SubStringSearch","l":"findIndexes()"},{"p":"ru.nsu.krasnikov","c":"SubStringSearch","l":"SubStringSearch(String, String)","url":"%3Cinit%3E(java.lang.String,java.lang.String)"}]
