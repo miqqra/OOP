adb8accc693593683460628a629ab72f0b36dd36
miqqra <mikhail.krasnikov.04@gmail.com>
2022-11-19 15:46:45 +0700

    discipline class now isn't a record

--- Task_1_4_1/src/main/java/ru/nsu/krasnikov/RecordBook.java
+++ Task_1_4_1/src/main/java/ru/nsu/krasnikov/RecordBook.java
@@ -48,7 +48,7 @@ public class RecordBook {
         return subjects
                 .stream()
                 .filter(x -> x.semester == semester)
-                .mapToDouble(Discipline::score)
+                .mapToDouble(Discipline::getScore)
                 .average()
                 .orElse(0);
     }
@@ -61,7 +61,7 @@ public class RecordBook {
     public Double getAverageScore() {
         return subjects
                 .stream()
-                .mapToDouble(Discipline::score)
+                .mapToDouble(Discipline::getScore)
                 .average()
                 .orElse(0);
     }
@@ -96,6 +96,19 @@ public class RecordBook {
                 .allMatch(x -> x.score > 3);
     }
 
-    private record Discipline(String name, int semester, int score) {
+    private static class Discipline{
+        private final String name;
+        private final int semester;
+        private final int score;
+
+        public Discipline(String name, int semester, int score){
+            this.name = name;
+            this.semester = semester;
+            this.score = score;
+        }
+
+        public int getScore(){
+            return this.score;
+        }
     }
 }
