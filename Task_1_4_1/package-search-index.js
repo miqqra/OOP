52a14952ab9bcca91334c6752d8052772e4016f8
miqqra <miqqra@users.noreply.github.com>
2022-11-20 09:57:09 +0000

    Deploying to gh-pages from @ miqqra/OOP@e994f0b6f1d22c4ecec3d2ac40c90dd1e0946438 🚀

--- Task_1_4_1/package-search-index.js
+++ Task_1_4_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.nsu.krasnikov"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.krasnikov"}];updateSearchResults();
