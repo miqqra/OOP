06c4439fe0f6ece0bdaf999d72b564c24a5c0dc0
miqqra <miqqra@users.noreply.github.com>
2022-12-12 04:29:50 +0000

    Deploying to gh-pages from @ miqqra/OOP@5a6ac9aad4e7c14dd5f50d3b390f20d238340e9e 🚀

--- Task_1_5_1/type-search-index.js
+++ Task_1_5_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.krasnikov","l":"Calculator"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.krasnikov","l":"Calculator"},{"p":"ru.nsu.krasnikov","l":"FunctionFactory"},{"p":"ru.nsu.krasnikov","l":"FunctionProperty"}];updateSearchResults();
