dbabc63193538ff376263c593ca90182f1e04c40
miqqra <miqqra@users.noreply.github.com>
2023-06-19 11:48:26 +0000

    Deploying to gh-pages from @ miqqra/OOP@22806c8b5b8d425351de1b9293a79ab6c0435cb3 🚀

---
+++ Task_2_3_1/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.krasnikov"},{"l":"ru.nsu.krasnikov.controller"},{"l":"ru.nsu.krasnikov.model"},{"l":"ru.nsu.krasnikov.view"}];updateSearchResults();
